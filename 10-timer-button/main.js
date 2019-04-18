8d57acf42362ef221d10601e34ebad087b0d80b5
wangzilong <1321787581@qq.com>
2019-04-18 16:15:35 +0800

    timer

---
+++ 10-timer-button/main.js
@@ -0,0 +1,17 @@
+$(function(){
+	var n=6;
+	var $btnAgree=$('input[type="button"]');
+	var timer=window.setInterval(function(){
+		n--;
+		if(n==0){
+			window.clearInterval(timer);
+			$btnAgree.removeAttr('disabled');
+			$btnAgree.val('同意');
+		}else{
+			$btnAgree.val('同意('+n+'s)');
+		}
+	},1000);
+	$btnAgree.click(function(){
+		alert('就知道你会点-form 顶哥')
+	});
+});
